ccef89a9881b162b80d1ac24d66d9eb5290f19e7
Christian Koep <ckoep@redhat.com>
2020-07-24 15:37:57 +0200

    webserver

---
+++ webserver/README.md
@@ -0,0 +1,1 @@
+[![Docker Repository on Quay](https://quay.io/repository/koep/webserver/status "Docker Repository on Quay")](https://quay.io/repository/koep/webserver)
---
+++ webserver/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", HelloServer)
+    http.ListenAndServe(":8080", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+}
